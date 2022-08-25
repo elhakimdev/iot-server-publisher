365bebc541aabf8fa1b43dcca3faeccb659da627
ElhakimDev <elhakimdev@ElhakimDev.localdomain>
2022-08-26 05:27:43 +0700

    test json terus

--- src/handler/publisher.ts
+++ src/handler/publisher.ts
@@ -9,87 +9,75 @@ const publisher = (...args) => {
     const port  = mqtt_config.port;
     const clientId      = `mqtt_${Math.random().toString(16).slice(3)}`;
     const connectUrl    = `mqtt://${host}:${port}`;
-    const payload = {
-        locoid: "CC202-11-11",
-        gpsdatetime: momentTZ.tz('Asia/jakarta').format('YYYY-MM-DD HH:mm:ss'),
-        longitude: faker.faker.address.longitude(112, 100, 5),
-        latitude: faker.faker.address.longitude(1, -0.1, 5),
-        speed: faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        heading: faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        tekmp: faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        tekap: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tekt: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tekcc: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tekbp: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tekbc: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tekmr: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tpse: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tpsete: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tpdsr: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tpdser: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        tengroom: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        fuel: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        voltmg: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm1: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm2: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm3: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm4: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm5: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        currtm6: faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        rpm: faker.faker.datatype.number({min: 1000, max: 100000, precision: 100 }),
-        throttle: faker.faker.datatype.number({ min:1, max: 8, precision: 1 }),
-        reverser: "FOR",
-        network: "TSEL 2G/3G/4G",
-        sigquality: faker.faker.datatype.number({min: 1, max: 5, precision: 1})
-    }
+    // const payload = {
+    //     locoid: "CC202-11-11",
+    //     gpsdatetime: momentTZ.tz('Asia/jakarta').format('YYYY-MM-DD HH:mm:ss'),
+    //     longitude: faker.faker.address.longitude(112, 100, 5),
+    //     latitude:parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     speed: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     heading: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekmp: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekap: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekt: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekcc: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekbp: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekbc: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tekmr: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tpse: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tpsete: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tpdsr: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tpdser: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     tengroom: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     fuel: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     voltmg: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm1: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm2: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm3: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm4: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm5: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     currtm6: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     rpm: parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    //     throttle: faker.faker.datatype.number({ min:1, max: 8, precision: 1 }),
+    //     reverser: "FOR",
+    //     network: "TSEL 2G/3G/4G",
+    //     sigquality: faker.faker.datatype.number({min: 1, max: 5, precision: 1})
+    // }  
 
-    // const stringify = (obj: Object) => {
-    //     // Iterate over keys, reducing to a string
-    //     let str = Object.keys(obj).reduce((acc, cur) => {
-    //         // @ts-ignore
-    //         let next = `${cur}: "${obj[cur]}"`;
-    //         return acc
-    //             ? `${acc}, ${next}`
-    //             : `{${next}`;
-    //     }, '');
-        
-    //     // Return, appending final '}'
-    //     return `${str}}`;
-    // }
-    
+    // momentTZ.tz('Asia/jakarta').format('YYYY-MM-DD HH:mm:ss
+
     const context = JSON.stringify({
         "locoid": "CC202-11-11",
-        "gpsdatetime": momentTZ.tz('Asia/jakarta').format('YYYY-MM-DD HH:mm:ss'),
-        "longitude": faker.faker.address.longitude(112, 100, 5),
-        "latitude": faker.faker.address.longitude(1, -0.1, 5),
-        "speed": faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        "heading": faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        "tekmp": faker.faker.datatype.float({min: 0, max:100, precision: 0.001 }),
-        "tekap": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tekt": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tekcc": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tekbp": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tekbc": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tekmr": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tpse": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tpsete": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tpdsr": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tpdser": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "tengroom": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "fuel": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "voltmg": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm1": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm2": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm3": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm4": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm5": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "currtm6": faker.faker.datatype.float({ min:0 , max:100, precision: 0.001 }),
-        "rpm": faker.faker.datatype.number({min: 1000, max: 100000, precision: 100 }),
-        "throttle": faker.faker.datatype.number({ min:1, max: 8, precision: 1 }),
+        "gpsdatetime": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "longitude": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "latitude":parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "speed": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "heading": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekmp": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekap": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekt": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekcc": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekbp": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekbc": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tekmr": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tpse": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tpsete": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tpdsr": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tpdser": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "tengroom": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "fuel": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "voltmg": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm1": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm2": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm3": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm4": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm5": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "currtm6": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "rpm": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+        "throttle": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
         "reverser": "FOR",
         "network": "TSEL4G",
-        "sigquality": faker.faker.datatype.number({min: 1, max: 5, precision: 1})
-    })
+        "sigquality": parseFloat((Math.random() * (3.5 - 1.5) + 1.5).toFixed(4)),
+    }, "", 4)
 
     const client = mqtt.connect(connectUrl, {
         clientId,
